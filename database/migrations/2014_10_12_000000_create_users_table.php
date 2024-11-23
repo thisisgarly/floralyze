8121c8a41579e165f0026c814e8b3f1a826e4318
thisisgarly <thisisgarly@gmail.com>
2024-11-23 20:07:24 +0700

    FIX : Model & Migration - User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id');
             $table->string('name');
+            $table->boolean('gender');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
