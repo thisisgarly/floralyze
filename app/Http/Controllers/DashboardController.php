0ca7002dddc75bfd7db381ff6eaba0ece49f3769
thisisgarly <thisisgarly@gmail.com>
2024-11-23 17:42:13 +0700

    FIX : View - Login

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $title = "Dashboard";
+        
+        return view('master.dashboard.index', compact(
+            'title',
+        ));
+    }
+}
